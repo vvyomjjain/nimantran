7d37f472b58f8bc73650a9de4bde091711782ce4
vvyomjjain <vvyomjjain@gmail.com>
2018-05-01 15:06:49 +0530

    correction in views.py

--- nimantran/catalog/views.py
+++ nimantran/catalog/views.py
@@ -90,6 +90,10 @@ def my_view(request):
         permission_required = ('catalog.can_mark_going', 'catalog.can_edit')
         # Note that 'catalog.can_edit' is just an example
         # the catalog application doesn't have such permission!
+
+class UserDetailView(generic.DetailView):
+    model = User
+    
 class UserDetailView(generic.DetailView):
     model = User
     template_name = 'catalog/user_detail.html'
