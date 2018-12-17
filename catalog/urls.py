89692e15f5e142fb7a7b6264aa578720fdd21873
vvyomjjain <vvyomjjain@gmail.com>
2018-12-17 15:49:30 +0530

    for heroku

---
+++ catalog/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+import uuid
+from django.conf.urls import url
+from . import views as core_views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+	path('events/', views.EventListView.as_view(), name='events'),
+	path('events/<int:pk>', views.EventDetailView.as_view(), name='event-detail'),
+    path('venue/<int:pk>', views.VenueDetailView.as_view(), name='venue-detail'),
+
+    path('myinvits/', views.InvitedListView.as_view(), name='my-invited'),
+    path('myinvits/<uuid:pk>', views.InvitedDetailView.as_view(), name='invite-detail'),
+    path('myorganized/', views.OrganizedListView.as_view(), name='my-organized'),
+    path('user/<int:pk>', views.UserDetailView.as_view(), name='profile'),
+
+    path('events/create/', views.EventCreate.as_view(), name = 'events_create'),
+    path('events/<int:pk>/update/', views.EventUpdate.as_view(), name='events_update'),
+    path('events/<int:pk>/delete', views.EventDelete.as_view(), name='events_delete'),
+
+    path('invitations/create/', views.InviteCreate.as_view(), name='invite_create'),
+    path('invitations/<uuid:pk>/update/', views.InviteUpdate.as_view(), name='invite_update'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ catalog/views.py
@@ -0,0 +1,137 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Event
+
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from .models import People, Venue, Event, Invitation
+from .forms import SelectResponse
+
+from django.views import generic
+
+def index(request):
+    """
+    View function for homepage
+    """
+    # generating counts of the some main objects
+    num_people = User.objects.all().count()
+    num_venue = Venue.objects.all().count()
+    num_event = Event.objects.all().count()
+
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
+    # render the html template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={'num_people':num_people, 'num_venue':num_venue, 'num_event':num_event, 'num_visits':num_visits},
+    )
+
+def about(request):
+    """
+    View function for about page
+    """
+
+    return render(
+        request,
+        'about.html',
+    )
+
+class EventListView(generic.ListView):
+    model = Event
+
+class EventDetailView(generic.DetailView):
+    model = Event
+
+class VenueDetailView(generic.DetailView):
+    model = Venue
+
+class InvitedListView(LoginRequiredMixin, generic.ListView):
+    model = Invitation
+    template_name = 'catalog/invited_list_user.html'
+
+    def get_queryset(self):
+        return Invitation.objects.filter(invitee=self.request.user)
+
+class InvitedDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Invitation
+    template_name = 'catalog/invitation_detail.html'
+
+class OrganizedListView(LoginRequiredMixin, generic.ListView):
+    model = Event
+    template_name = 'catalog/organized_list_user.html'
+
+    def get_queryset(self):
+        return Event.objects.filter(organizer=self.request.user)
+
+from django.contrib.auth.decorators import permission_required
+
+@permission_required('catalog.can_mark_going')
+@permission_required('catalog.can_edit')
+def my_view(request):
+
+    from django.contrib.auth.mixins import PermissionRequiredMixin
+
+    class MyView(PermissionRequiredMixin, View):
+        permission_required = 'catalog.can_mark_going'
+        # Or multiple permissions
+        permission_required = ('catalog.can_mark_going', 'catalog.can_edit')
+        # Note that 'catalog.can_edit' is just an example
+        # the catalog application doesn't have such permission!
+
+class UserDetailView(generic.DetailView):
+    model = User
+    
+class UserDetailView(generic.DetailView):
+    model = User
+    template_name = 'catalog/user_detail.html'
+
+class EventCreate(LoginRequiredMixin, CreateView):
+    model = Event
+    fields = '__all__'
+
+class EventUpdate(LoginRequiredMixin, UpdateView):
+    model = Event
+    fields = '__all__'
+
+class EventDelete(LoginRequiredMixin, DeleteView):
+    model = Event
+    success_url = reverse_lazy('events')
+
+class InviteUpdate(LoginRequiredMixin, UpdateView):
+    model = Invitation
+    fields = ['status']
+
+class InviteCreate(LoginRequiredMixin, CreateView):
+    model = Invitation
+    fields = ['event', 'invitee', 'note']
+
+class InvitationCreate(LoginRequiredMixin, CreateView):
+    model = Invitation
+    fields = ['event', 'invitee', 'note']
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
